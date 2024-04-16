2a3ca90cc7653179ce3536aebe277d88fdeb2303
ejcanoy <euan.james.canoy@gmail.com>
2024-04-16 13:54:30 -0700

    updated and added game routes

--- backend/controllers/gameController.js
+++ backend/controllers/gameController.js
@@ -6,9 +6,8 @@ exports.game_get_all = asyncHandler(async (req, res) => {
     try {
         const result = await Game.find({}).exec();
         if (!result) {
-            return res.status(404).send("Map not found");
+            return res.status(404).send("Games not found");
         }
-        console.log(result);
         return res.send(result);
     } catch (error) {
         console.error(error);
@@ -16,13 +15,43 @@ exports.game_get_all = asyncHandler(async (req, res) => {
     }
 });
 
+exports.game_get = asyncHandler(async (req,res) => {
+    try {
+        const result = await Game.findById(req.params.mapid);
+        if (!result) {
+            return res.status(404).send("Game not found");
+        }
+        return res.send(result);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send("Internal Server Error");
+    }
+})
+
 exports.game_post = asyncHandler(async (req,res) => {
     const game = new Game({
-        name: req.body.name,
-        startTime: req.body.startTime,
-        endTime: req.body.endTime
+        name: null,
+        startTime: new Date(),
+        endTime: null
     })
 
     const result = await game.save();
     return res.send(result);
+})
+
+exports.game_put = asyncHandler(async (req, res) => {
+    const game = await Game.findByIdAndUpdate(req.params.mapid, {name: req.body.name, endTime: new Date()}, {new: true});
+
+    if (!game) {
+        const err = new Error("Game not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    return res.send(game);
+})
+
+exports.game_delete = asyncHandler(async (req, res) => {
+    const game = await Game.findByIdAndDelete(req.params.mapid);
+    return res.send(game);
 })
--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -4,7 +4,10 @@ const game_controller = require('../controllers/gameController');
 
 
 router.get('/' , game_controller.game_get_all);
+router.get('/:mapid', game_controller.game_get);
 router.post('/', game_controller.game_post);
+router.put('/:mapid', game_controller.game_put);
+router.delete('/:mapid', game_controller.game_delete);
 
 
 

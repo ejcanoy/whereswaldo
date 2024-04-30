551a6f0b5614a8f20a66714d12227bdac910ea92
ejcanoy <euan.james.canoy@gmail.com>
2024-04-30 16:50:56 -0700

    deleted extra console.logs

--- backend/controllers/mapController.js
+++ backend/controllers/mapController.js
@@ -3,12 +3,10 @@ const asyncHandler = require("express-async-handler")
 
 exports.map_get = asyncHandler(async (req, res) => {
     try {
-        console.log(req.params.mapid);
         const result = await Map.findById(req.params.mapid).exec();
         if (!result) {
             return res.status(404).send("Map not found");
         }
-        console.log(result);
         return res.send(result);
     } catch (error) {
         console.error(error);
@@ -19,12 +17,10 @@ exports.map_get = asyncHandler(async (req, res) => {
 
 exports.map_get_all = asyncHandler(async (req, res) => {
     try {
-        console.log(req.params.mapid);
         const result = await Map.find({}).exec();
         if (!result) {
             return res.status(404).send("Map not found");
         }
-        console.log(result);
         return res.send(result);
     } catch (error) {
         console.error(error);

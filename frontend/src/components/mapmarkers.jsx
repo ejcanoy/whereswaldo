40b902c98a87037019a1d6a32833a8ab0871eec7
ejcanoy <euan.james.canoy@gmail.com>
2024-05-03 12:19:04 -0700

    added logic to handlenewgamebutton

--- frontend/src/components/mapmarkers.jsx
+++ frontend/src/components/mapmarkers.jsx
@@ -182,23 +182,22 @@ function MapMarkers() {
   }
 
   async function handleNewGameButton() {
-    console.log("Clicked new game");
-    // try {
-    //   const response = await fetch(`${apiURL}/game`, {
-    //     mode: "cors",
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json"
-    //     },
-    //     body: JSON.stringify({ "mapid": mapid })
-    //   });
-    //   const data = await response.json();
-    //   localStorage.setItem("gameId", data._id);;
-    //   setGameOver(false);
-    //   setShowNewGame(false);
-    // } catch (error) {
-    //   console.error("Could not start new game", error);
-    // }
+    try {
+      const response = await fetch(`${apiURL}/game`, {
+        mode: "cors",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ "mapid": mapid })
+      });
+      const data = await response.json();
+      localStorage.setItem("gameId", data._id);;
+      setGameOver(false);
+      setShowNewGame(false);
+    } catch (error) {
+      console.error("Could not start new game", error);
+    }
 
   }
 
